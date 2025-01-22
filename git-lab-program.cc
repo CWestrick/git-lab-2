d61a13fc47fb87cc053bed3db0d4257ad9c9d8a5
CWestrick <connorwestrick12@gmail.com>
2025-01-22 17:34:07 -0500

    commited

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Connor Westrick
+ *        Date: 1/22/2025
+ * Description: addint code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "hello Git!!" << endl;
+    return 0;
+}// main
